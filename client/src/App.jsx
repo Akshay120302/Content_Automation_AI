0363257f72b79accdca17f795644a40571ced5bf
Akshay120302 <akshay.sinha335@gmail.com>
2026-01-08 21:10:04 +0530

    Initial structure setup successful, ready for further development 08-01-2026 9:10

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./components/Home";
+
+const App = () => (
+    <Router>
+
+      <Routes>
+
+        <Route path = "/" element = {<Home/>}/>
+
+      </Routes>
+
+    </Router>
+  
+);
+
+export default App
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import logo from "../assets/FlowForge1.png";
+
+const Home = () => {
+  return (
+    <>
+      <h1 className="text-3xl text-green-600 font-bold">
+  Tailwind + React working 🚀
+</h1>
+    </>
+  );
+};
+
+export default Home;
